64cc7306d200ba64f939ad1d32c9429a57069e3b
Lakshay Kumar <91063764+Lakshay9296@users.noreply.github.com>
2024-08-30 16:54:00 +0530

    Add files via upload

--- about.py
+++ about.py
@@ -1,15 +1,21 @@
 import streamlit as st
+from streamlit_pdf_viewer import pdf_viewer
 
 col1,col2 = st.columns(2,gap='medium',vertical_alignment='center')
+resume = "assets/resume.pdf"
 
 with col1:
     st.image("assets/profile.png")
     
 with col2:
     st.title("Lakshay Kumar",anchor=False)
-    st.write("Data Analyst")
+    st.subheader("Data Analyst",anchor=False)
     st.write("I'm Lakshay Kumar, a BE graduate in Computer Science and Engineering from Chandigarh University. I'm passionate about data science, Python programming, and building practical applications.")
-
+    with open(resume, "rb") as f:
+      binary_data = f.read()
+    st.download_button("Download Resume",data=binary_data,file_name="resume.pdf")
+    
+    
 st.write("\n")
 st.subheader("Technical Skills",anchor=False)
 st.write(
@@ -40,5 +46,15 @@ st.write(
       **Percentage:** 83.5%
     """
 )
+st.write("\n")
+st.write("\n")
+
+
+resume_button = st.button("Show Resume")
+if resume_button:
+  if st.button("Hide Resume"):
+    resume_button=False
+  pdf_viewer(input=binary_data, width=700)
+
 
 
